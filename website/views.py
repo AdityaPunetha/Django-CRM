dfa8225a20c31d07236ac5d63d0e2e3acba8c28b
AdityaPunetha <71438448+AdityaPunetha@users.noreply.github.com>
2023-04-03 17:36:44 +0530

    registration complete

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from website.forms import SignUpForm
 
 # Create your views here.
 
@@ -31,4 +32,17 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, "register.html", {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, f"Welcome {username}!")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
+    return render(request, "register.html", {"form": form})
